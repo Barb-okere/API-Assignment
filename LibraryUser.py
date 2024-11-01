5efbdf7942cc17a9358788eab95072722b8b3a98
Barb <barbara.oluoch@strathmore.edu>
2024-11-01 21:41:05 +0300

    borrow book and add it to library users borrowed books

--- LibraryUser.py
+++ LibraryUser.py
@@ -4,3 +4,12 @@ class LibraryUser:
         self.name = name
         self.borrowed_books = []
     #Represents a library user and manages the user's borrowed books. 
+    #The borrowed_books attribute is a list to store the books borrowed by the user.
+
+    # Borrow a book and add it to the user's borrowed books list 
+    def borrow_book(self, book):
+        try:
+            book.borrow_book()
+            self.borrowed_books.append(book)
+        except Exception as e: # error handling
+            print(f"Error: {e}")
