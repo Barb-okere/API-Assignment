387836a98925e056cd3558c8d2b2a210fc36cd11
Barb <barbara.oluoch@strathmore.edu>
2024-11-01 20:51:38 +0300

    book class contains title, author

--- Book.py
+++ Book.py
@@ -1,2 +1,11 @@
 from abc import ABC, abstractmethod
 
+# Base class for all book types
+class Book(ABC):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def display(self):
+        pass
