02199e96dcfcdd625f119754075da79a8aaac3fd
Barb <barbara.oluoch@strathmore.edu>
2024-11-01 21:49:41 +0300

    library class manages the library's collection of books

---
+++ Library.py
@@ -0,0 +1,5 @@
+# Library class to manage book collection
+class Library:
+    def __init__(self):
+        self.books = []  # List to hold books in the library
+    #Manages the library's collection of books. 
